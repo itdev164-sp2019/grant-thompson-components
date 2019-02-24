cab09ab378027185e69d8c193ffa0740211345bc
Grant Thompson <thompsgh@gmatc.matc.edu>
2019-02-24 13:14:38 -0600

    configured Bit and added components

--- components/src/layouts/layout.js
+++ components/src/layouts/layout.js
@@ -6,6 +6,7 @@ import "./layout.css"
 import { ThemeProvider } from 'styled-components'
 import {Gray as theme } from '../themes/Gray'
 import { Footer, Main} from '../components/Element'
+import {Masthead} from '../components/Masthead'
 
 const Layout = ({ children }) => (
   <ThemeProvider theme={theme}>
@@ -20,8 +21,8 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
+      <div>
+        <Masthead siteTitle={data.site.siteMetadata.title} height="75px"/>
         <div
           style={{
             margin: `0 auto`,
@@ -37,7 +38,7 @@ const Layout = ({ children }) => (
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </Footer>
         </div>
-      </>
+      </div>
     )}
   />
   </ThemeProvider>
