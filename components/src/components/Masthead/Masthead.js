51b5d296820e8fde310896c3e5d70f73bc2da88e
Grant Thompson <thompsgh@gmatc.matc.edu>
2019-03-10 08:50:26 -0500

    added DefaultLayout to Masthead

--- components/src/components/Masthead/Masthead.js
+++ components/src/components/Masthead/Masthead.js
@@ -2,29 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Box} from 'rebass'
 import styled from 'styled-components'
-import { Link } from 'rebass'
+//import { Link } from 'rebass'
 
 const Outer = styled(Box)`
 height: ${props => props.height};
 width: 100%;
-background-color: rebeccapurple;
-margin-bottom: 1.45rem;
-`
+position: fixed;
+top: 0px;
+z-index: 9999;
+`;
 
-const StyledLink = styled(Link)`
-color: white;
-text-decoration: none;
-`
+// const StyledLink = styled(Link)`
+// color: white;
+// text-decoration: none;
+// `
 
-export const Masthead = ({ height, siteTitle}) => {
+export const Masthead = ({ height, children}) => {
     return (
         <Outer height={height}>
-            <StyledLink href="/">{siteTitle}</StyledLink>
+            {children}
         </Outer>
     )
-}
+};
 
 Masthead.protoTypes = {
     height: PropTypes.string.isRequired,
-    siteTitle: PropTypes.string
-}
+    children: PropTypes.node.isRequired
+};
